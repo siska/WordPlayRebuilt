912763ef879cd37f416943d6800cb29986ad756b
Ryan Siska <rsiska1@gmail.com>
2015-01-12 21:30:01 -0600

    Completed Storyboard setup - adding segues next

---
+++ WordPlayRebuilt/EnterNameViewController.m
@@ -0,0 +1,38 @@
+//
+//  EnterNameViewController.m
+//  WordPlayRebuilt
+//
+//  Created by S on 1/12/15.
+//  Copyright (c) 2015 Ryan Siska. All rights reserved.
+//
+
+#import "EnterNameViewController.h"
+
+@interface EnterNameViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+
+@end
+
+@implementation EnterNameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ WordPlayRebuilt/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  WordPlayRebuilt
+//
+//  Created by S on 1/12/15.
+//  Copyright (c) 2015 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
