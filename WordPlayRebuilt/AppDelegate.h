458a1248d500a6faf556f95e6889583ea9315117
Ryan Siska <rsiska1@gmail.com>
2015-01-12 19:42:30 -0600

    Initial Commit

---
+++ WordPlayRebuilt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPlayRebuilt
+//
+//  Created by S on 1/12/15.
+//  Copyright (c) 2015 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPlayRebuilt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlayRebuilt
+//
+//  Created by S on 1/12/15.
+//  Copyright (c) 2015 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
